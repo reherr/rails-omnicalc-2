d47bf435b18187233e7934513c2eb98572bdb2a7
Rebecca Herrera <133678924+reherr@users.noreply.github.com>
2024-06-04 17:26:31 +0000

    fixed division result

--- app/controllers/division_controller.rb
+++ app/controllers/division_controller.rb
@@ -7,7 +7,19 @@ class DivisionController < ApplicationController
     @first_number = params.fetch("first_num")
     @second_number = params.fetch("second_num").to_f
 
-    @result =  first_number / @second_number
+    @result =  @first_number / @second_number
+
+    if @second_num != 0
+      if (@first_num % @second_num) == 0
+        puts @first_num / @second_num
+      else
+        @result = @first_num / @second_num
+        @formatted_result = sprintf("%.16f", @result)
+        puts @formatted_result
+      end
+    else
+      puts "Infinity"
+    end
 
     render({ :template => "division_templates/divide_results" })
   end
